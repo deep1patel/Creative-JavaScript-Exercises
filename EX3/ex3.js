5a8984237129341a6a68ba00031d759174fd9bd6
deep1patel <54338389+deep1patel@users.noreply.github.com>
2021-03-25 19:41:01 -0400

    Update ex3.js

--- EX3/ex3.js
+++ EX3/ex3.js
@@ -1,30 +1,28 @@
 function palinDetermine() { 
 
-    var numberEntry = document.getElementById("userInput").value; 
-   
-    var tempNumber = numberEntry; 
+  var numberEntry = document.getElementById("userInput").value; 
+ 
+  var tempNumber = numberEntry; 
 
-    var variableNumber; 
+  var variableNumber; 
 
-    var reverseNumber = 0;
-   
-    var palindromeAnswer; 
-   
+  var reverseNumber = 0;
+ 
+  var palindromeAnswer; 
+ 
 
-    while(numberEntry > 0){
-      variableNumber = numberEntry % 10;
-      reverseNumber = (reverseNumber * 10) + variableNumber;
-      numberEntry = parseInt(numberEntry / 10);}
-
-
-    if(reverseNumber == tempNumber ){
-      palindromeAnswer = "yes";}
+  while(numberEntry > 0){
+    variableNumber = numberEntry % 10;
+    reverseNumber = (reverseNumber * 10) + variableNumber;
+    numberEntry = parseInt(numberEntry / 10);}
 
 
-    else if(reverseNumber != tempNumber){
-      palindromeAnswer = "no";}
+  if(reverseNumber == tempNumber ){
+    palindromeAnswer = "yes";}
 
 
-      document.getElementById("palin").innerHTML = palindromeAnswer;
-     
-}
+  else if(reverseNumber != tempNumber){
+    palindromeAnswer = "no";}
+
+
+    document.getElementById("palin").innerHTML = palindromeAnswer;
