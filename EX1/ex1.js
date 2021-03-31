8025408ebbd146dfd70bd42a71a9b06026a34044
raveena-gr <78176644+raveena-gr@users.noreply.github.com>
2021-03-31 16:18:41 -0500

    added e for 40-50 mark

--- EX1/ex1.js
+++ EX1/ex1.js
@@ -5,52 +5,49 @@ function gradeMapper(){
     var userInput = document.getElementById("num1").value
     var errorPrompt = "";
 
-    if (isNaN(userInput) || userInput < 1 || userInput > 100) {
-        errorPrompt += "INVALID ENTRY. Enteries must be between 1 to 100"
-    }
+    if (isNaN(userInput) || userInput < 0 || userInput > 100) {
+        errorPrompt += "INVALID ENTRY. Enteries must be between 1 to 100"}
     
     if (errorPrompt != "") {
-        window.alert(errorPrompt);
-    }
+        window.alert(errorPrompt);}
 
     else {
         var gradeLetter;
-        switch(Math.trunc((userInput*2)/10)){
-            case 20:
-            case 19:
-            case 18:
-                gradeLetter = " A+ ";
-                break;
-            case 17:
-            case 16:
-                gradeLetter = " A ";
-                break;
-            case 15:
-                gradeLetter = " B+ ";
-                break;
-            case 14:
-                gradeLetter = " B ";
-                break;
-            case 13:
-                gradeLetter = " C+ ";
-                break;
-            case 12:
-                gradeLetter = " C ";
-                break;
-            case 11:
-                gradeLetter = " D+ ";
-                break;
-            case 10:
-                gradeLetter = " D ";
-                break;
-            case 9:
-            case 8:
-            
-            default:
-                gradeLetter = " F ";
-                break;
-        } 
+       
+        if (userInput < 40 && userInput >= 0) {
+        gradeLetter = "F";} 
+        
+        else if ( userInput >= 40 && userInput <= 49 ){
+        gradeLetter = "E";}
+
+        else if ( userInput >= 50 && userInput <= 54 ){
+        gradeLetter = "D";}
+
+        else if ( userInput >= 55 && userInput <= 59 ){
+        gradeLetter = "D+";}
+
+        else if ( userInput >= 60 && userInput <= 64 ){
+        gradeLetter = "C";}
+
+        else if ( userInput >= 65 && userInput <= 69 ){
+        gradeLetter = "C+";}
+
+        else if ( userInput >= 70 && userInput <= 74 ){
+        gradeLetter = "B";}
+
+        else if ( userInput >= 75 && userInput <= 79 ){
+        gradeLetter = "B+";}
+
+        else if ( userInput >= 80 && userInput <= 89 ){
+        gradeLetter = "A";}
+
+        else if ( userInput >= 90 && userInput <= 100 ){
+        gradeLetter = "A+";}
+
+        else {
+        window.alert("INVALID ENTRY. Enteries must be between 0 to 100");
+        gradeLetter = ""}
 
         document.getElementById("covertedGrade").innerText = gradeLetter;
-    }
+        }
 }
